1213ba102afbb25adc1376057d7e2148b702deda
Florent Blot <mail.de.florent@gmail.com>
2019-01-25 23:05:55 +0100

    change root api routes

--- src/main/kotlin/app/api/ApiController.kt
+++ src/main/kotlin/app/api/ApiController.kt
@@ -14,19 +14,19 @@ class ApiController {
     private val carRoute = CarRoute()
 
     init {
-        get("/") { _, _ -> renderWelcomeText() }
+        get("/") { _, _ -> renderWelcome() }
 
         path("/api") {
-            setupConfigs()
-            setupApi()
-            setupPaths()
-            setupErrors()
+            setConfigs()
+            setApi()
+            setPaths()
+            setErrors()
         }
     }
 
-    private fun setupConfigs() {
-        before("*") {  _, _ -> checkAuthenticate() }
-        after("*") { _, res -> res.type(RES_TYPE_JSON) }
+    private fun setConfigs() {
+        before("/*") {  _, _ -> checkAuthenticate() }
+        after("/*") { _, res -> res.type(RES_TYPE_JSON) }
     }
 
     private fun checkAuthenticate() {
@@ -36,25 +36,25 @@ class ApiController {
         }
     }
 
-    private fun setupApi() {
-        get("") { _, _ -> renderApiJson() }
+    private fun setApi() {
+        get("/") { _, _ -> renderApi() }
     }
 
-    private fun setupPaths() {
+    private fun setPaths() {
         path("/pilots") { pilotRoute.getRoutes() }
         path("/cars") { carRoute.getRoutes() }
     }
 
-    private fun setupErrors() {
-        notFound(renderNotFoundJson())
-        internalServerError(renderServerErrorJson())
+    private fun setErrors() {
+        notFound(renderNotFound())
+        internalServerError(renderServerError())
     }
 
-    private fun renderWelcomeText() = "Welcome to Back Your Future!"
+    private fun renderWelcome() = "<html><body><h1>Welcome travellers!</h1></body></html>"
 
-    private fun renderApiJson() = "{ \"text\": \"Welcome to my api! You are authenticated.\" }"
+    private fun renderApi() = "{ \"text\": \"Great Scott! You are authenticated.\" }"
 
-    private fun renderNotFoundJson() = "{ \"error\": 404, \"text\": \"Oops, the DeLoran is not here!\" }"
+    private fun renderNotFound() = "{ \"error\": 404, \"text\": \"Oops, the DeLoran is not here!\" }"
 
-    private fun renderServerErrorJson() = "{ \"error\": 500, \"text\": \"Aouch! You broke the space-time continuum...\" }"
+    private fun renderServerError() = "{ \"error\": 500, \"text\": \"Aouch! You broke the space-time continuum...\" }"
 }
